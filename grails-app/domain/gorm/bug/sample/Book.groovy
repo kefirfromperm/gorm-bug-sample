1bc2299320e3c09153dc717a6b94159e5225c205
Vitalii Samolovskikh <vsamolovskikh@scentbird.com>
2018-03-26 08:53:50 +0500

    domain class and data service

---
+++ grails-app/domain/gorm/bug/sample/Book.groovy
@@ -0,0 +1,8 @@
+package gorm.bug.sample
+
+class Book {
+    String title
+
+    static constraints = {
+    }
+}
---
+++ grails-app/services/gorm/bug/sample/BookService.groovy
@@ -0,0 +1,10 @@
+package gorm.bug.sample
+
+import grails.gorm.services.Query
+import grails.gorm.services.Service
+
+@Service(Book)
+interface BookService {
+    @Query("update ${Book book} set ${book.title} = $title where ${book.id} = $id")
+    void update(long id, String title)
+}
